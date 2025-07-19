050316391842d536c9fee708899ce0765ab7cc9f
nowail <nov.za.x1@gmail.com>
2025-07-19 18:51:20 -0400

    Fix CORS configuration to allow Netlify frontend requests

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://*.netlify.app', 'http://localhost:5173', 'http://localhost:3000'
+    origins '*'  # Allow all origins for now to fix the issue
     
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: false
+      credentials: false,
+      expose: ['Content-Type', 'Content-Length']
   end
 end 
